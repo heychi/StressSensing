8fecce193402e9d4cdb7990acac3d1b551268e39
ChiyunSong <heychi0728@gmail.com>
2025-06-06 18:40:56 +0900

    fix: 카메라 뷰어 수정

--- src/components/CameraView.tsx
+++ src/components/CameraView.tsx
@@ -7,14 +7,28 @@ const CameraView: React.FC = () => {
     const videoRef = useCamera();
 
     return (
-        <div style={{ width: '100vw', height: '100vh', position: 'relative', backgroundColor: '#000' }}>
+        <div
+            style={{
+                width: '100vw',
+                height: '100vh',
+                position: 'relative',
+                backgroundColor: '#000',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                paddingTop: '60px',
+                boxSizing: 'border-box',
+            }}
+        >
             <Header />
             <video
                 ref={videoRef}
                 style={{
-                    width: '100%',
-                    height: '100%',
+                    width: '90vw',
+                    height: '75vh',
+                    borderRadius: '12px',
                     objectFit: 'cover',
+                    boxShadow: '0 0 10px rgba(255,255,255,0.1)',
                 }}
                 autoPlay
                 playsInline
